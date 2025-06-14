54dc83c9ac14a85680b062dde35ad3737e2d68d7
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-14 21:06:03 +0200

    create a window

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+using namespace std;
+
+
+int main () {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+
+    GLFWwindow* window = glfwCreateWindow(1080, 720, "Chess", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+}
