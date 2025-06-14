93bd72839e09ba90d45d8231b87272780cdbab79
thelastgoth128 <thindwacyrus86@gmail.com>
2025-06-14 21:19:53 +0200

    added a core profile

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main () {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(1080, 720, "Chess", NULL, NULL);
     if (window == NULL) {
