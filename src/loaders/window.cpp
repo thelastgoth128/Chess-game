4102c1b48fa260d56c2f5bcbef9c949edb42854a
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-12 15:25:39 +0200

    create and load window

--- src/loaders/window.cpp
+++ src/loaders/window.cpp
@@ -5,4 +5,13 @@ void Window::launch_glfw_renderer(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600,"LingoHands", NULL, NULL);
+    if ( window == NULL ) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return;
+    }
+    glfwMakeContextCurrent(window);
+    
 }
