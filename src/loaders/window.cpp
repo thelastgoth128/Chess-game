3dbc5ca94394f11064e7abed51b34a522e04acdc
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-12 15:20:16 +0200

    initialize glfw libraries

---
+++ src/loaders/window.cpp
@@ -0,0 +1,8 @@
+#include "window.h"
+
+void Window::launch_glfw_renderer(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_ANY_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
