7303e194e38be3e792a188c76beeee98642e4430
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-14 18:57:22 +0200

    move buffers to mesh

--- src/window.cpp
+++ src/window.cpp
@@ -20,15 +20,3 @@ void Window::launch_glfw_renderer(){
 
     glViewport(0, 0, 800, 600);
 }
-
-void Window::initialize_buffers() {
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    //bind the vertex Array Object first, then bind and set vertext buffer(s), and then configure vertex attributes
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), )
-}
