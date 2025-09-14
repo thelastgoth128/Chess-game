38db9027a412a967855226c07c2849dd1a33656f
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-14 18:42:49 +0200

    shader implementaion

--- src/shaders/shader.h
+++ src/shaders/shader.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <glad/glad.h>
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
