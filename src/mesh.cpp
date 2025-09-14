831c1cf8f12ff3af416dd616d16ed474b77813e8
thelastgoth128 <thindwacyrus86@gmail.com>
2025-09-14 18:26:41 +0200

    constructor class

---
+++ src/mesh.cpp
@@ -0,0 +1,9 @@
+#include "mesh.h"
+
+Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures) {
+    this->vertices = vertices;
+    this->indices = indices;
+    this->textures = textures;
+
+    setupMesh();
+}
